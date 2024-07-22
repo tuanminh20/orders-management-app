30e376a5886bfbd0c91e4eb5e3d3f1eee58f20c4
tuanminh20 <nguyen.tuan.minh@moneyforward.vn>
2024-07-22 11:39:58 +0700

    refactor: Remove commented out code for including customer and tags in getOrders function

--- app/models/Order.server.js
+++ app/models/Order.server.js
@@ -3,10 +3,10 @@ import db from "../db.server";
 export async function getOrders() {
   const orders = await db.order.findMany({
     orderBy: { id: "desc" },
-    include: {
-      customer: true,
-      tags: true,
-    },
+    // include: {
+      // customer: true,
+      // tags: true,
+    // },
   });
 
   if (orders.length === 0) return [];
